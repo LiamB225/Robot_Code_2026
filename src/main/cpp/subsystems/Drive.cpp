ccc39ee3eca9f82f3160266d67018aeb5d2f709b
Uriah Wales <uriah.j.wales@gmail.com>
2026-02-13 17:23:12 -0600

    finished swerve drive code

--- src/main/cpp/subsystems/Drive.cpp
+++ src/main/cpp/subsystems/Drive.cpp
@@ -7,9 +7,21 @@
 Drive::Drive() = default;
 
 // This method will be called once per scheduler run
-void Drive::Periodic() {}
+void Drive::Periodic() {
+    frc::Rotation2d angle{gyro.GetAngle()};
+    m_poseEstimator.Update(angle, {
+    frc::SwerveModulePosition{(units::meter_t)(frontleftDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(frontleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(frontrightDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(frontrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(backleftDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(backleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(backrightDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(backrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)}
+    });
+}
 
-frc::CommandPtr Drive::get_drive_command(
+frc2::CommandPtr Drive::get_drive_command(
     std::function<double(void)> get_drive_power,
     std::function<double(void)> get_strafe_power,
     std::function<double(void)> get_rot_power
@@ -21,11 +33,11 @@ frc::CommandPtr Drive::get_drive_command(
             units::radians_per_second_t rot;
 
             // Fill these variables with user input.
-            drive = (units::meters_per_second_t)get_drive_power();
-            strafe = (units::meters_per_second_t)get_strafe_power();
-            rot = (units::meters_per_second_t)get_rot_power();
+            drive = XRateLimiter.Calculate(frc::ApplyDeadband(get_drive_power(), 0.05)) * kRobotMaxSpeed;
+            strafe = YRateLimiter.Calculate(frc::ApplyDeadband(get_strafe_power(), 0.05)) * kRobotMaxSpeed;
+            rot = RotRateLimiter.Calculate(frc::ApplyDeadband(get_rot_power(), 0.05)) * kRobotRotMaxSpeed;
             
-            SwerveDrive(drive, strafe, rot);
+            SwerveDrive(drive, strafe, rot, false);
         }
     );
 }
@@ -33,7 +45,78 @@ frc::CommandPtr Drive::get_drive_command(
 void Drive::SwerveDrive(
     units::meters_per_second_t xspeed,
     units::meters_per_second_t yspeed,
-    units::radians_per_second_t rotspeed
+    units::radians_per_second_t rotspeed,
+    bool fieldRelative
 ) {
+    frc::ChassisSpeeds speeds{};
+    if(frc::DriverStation::GetAlliance() == frc::DriverStation::Alliance::kRed && fieldRelative) {
+        speeds = {-xspeed, -yspeed, rotspeed};
+    }
+    else {
+        speeds = {xspeed, yspeed, rotspeed};
+    }
+    frc::Rotation2d angle{};
+    angle = m_poseEstimator.GetEstimatedPosition().Rotation();
+    auto states = m_kinematics.ToSwerveModuleStates(frc::ChassisSpeeds::Discretize(
+        fieldRelative ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(speeds, angle) : speeds,
+        0.02_s));
+    m_kinematics.DesaturateWheelSpeeds(&states, kModuleMaxSpeed);
+    auto [fl, fr, bl, br] = states;
+    
+    frc::Rotation2d flEncoderRotation{(units::radian_t)(frontleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)};
+    frc::Rotation2d frEncoderRotation{(units::radian_t)(frontrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)};
+    frc::Rotation2d blEncoderRotation{(units::radian_t)(backleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)};
+    frc::Rotation2d brEncoderRotation{(units::radian_t)(backrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)};
+
+    fl.Optimize(flEncoderRotation);
+    fr.Optimize(frEncoderRotation);
+    bl.Optimize(blEncoderRotation);
+    br.Optimize(brEncoderRotation);
+
+    fl.CosineScale(flEncoderRotation);
+    fr.CosineScale(frEncoderRotation);
+    bl.CosineScale(blEncoderRotation);
+    br.CosineScale(brEncoderRotation);
+
+    //Drive Control
+    units::volt_t flDrivePIDVoltage = (units::volt_t)(flDrivePID.Calculate(
+        frontleftDriveMotor.GetEncoder().GetVelocity() * M_PI * 0.1016 / (8.14 * 60), fl.speed.value()));
+    units::volt_t frDrivePIDVoltage = (units::volt_t)(frDrivePID.Calculate(
+        frontrightDriveMotor.GetEncoder().GetVelocity() * M_PI * 0.1016 / (8.14 * 60), fr.speed.value()));
+    units::volt_t blDrivePIDVoltage = (units::volt_t)(blDrivePID.Calculate(
+        backleftDriveMotor.GetEncoder().GetVelocity() * M_PI * 0.1016 / (8.14 * 60), bl.speed.value()));
+    units::volt_t brDrivePIDVoltage = (units::volt_t)(brDrivePID.Calculate(
+        backrightDriveMotor.GetEncoder().GetVelocity() * M_PI * 0.1016 / (8.14 * 60), br.speed.value()));
+
+    units::volt_t flDriveFFVoltage = flDriveFF.Calculate(fl.speed);
+    units::volt_t frDriveFFVoltage = frDriveFF.Calculate(fr.speed);
+    units::volt_t blDriveFFVoltage = blDriveFF.Calculate(bl.speed);
+    units::volt_t brDriveFFVoltage = brDriveFF.Calculate(br.speed);
+
+    frontleftDriveMotor.SetVoltage(flDrivePIDVoltage + flDriveFFVoltage);
+    frontrightDriveMotor.SetVoltage(frDrivePIDVoltage + frDriveFFVoltage);
+    backleftDriveMotor.SetVoltage(blDrivePIDVoltage + blDriveFFVoltage);
+    backrightDriveMotor.SetVoltage(brDrivePIDVoltage + brDriveFFVoltage);
+
+    //Rotation Control
+    units::volt_t flRotPIDVoltage = (units::volt_t)(flRotPID.Calculate(
+        (units::radian_t)(frontleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2), fl.angle.Radians()));
+    units::volt_t frRotPIDVoltage = (units::volt_t)(frRotPID.Calculate(
+        (units::radian_t)(frontrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2), fr.angle.Radians()));
+    units::volt_t blRotPIDVoltage = (units::volt_t)(blRotPID.Calculate(
+        (units::radian_t)(backleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2), bl.angle.Radians()));
+    units::volt_t brRotPIDVoltage = (units::volt_t)(brRotPID.Calculate(
+        (units::radian_t)(backrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2), br.angle.Radians()));
+
+    units::volt_t flRotFFVoltage = flRotFF.Calculate(flRotPID.GetSetpoint().velocity);
+    units::volt_t frRotFFVoltage = frRotFF.Calculate(frRotPID.GetSetpoint().velocity);
+    units::volt_t blRotFFVoltage = blRotFF.Calculate(blRotPID.GetSetpoint().velocity);
+    units::volt_t brRotFFVoltage = brRotFF.Calculate(brRotPID.GetSetpoint().velocity);
+
+    frontleftRotMotor.SetVoltage(flRotPIDVoltage + flRotFFVoltage);
+    frontrightRotMotor.SetVoltage(frRotPIDVoltage + frRotFFVoltage);
+    backleftRotMotor.SetVoltage(blRotPIDVoltage + blRotFFVoltage);
+    backrightRotMotor.SetVoltage(brRotPIDVoltage + brRotFFVoltage);
+
     return;
 }
--- src/main/include/subsystems/Drive.h
+++ src/main/include/subsystems/Drive.h
@@ -14,6 +14,8 @@
 #include <frc/filter/SlewRateLimiter.h>
 #include <frc/estimator/SwerveDrivePoseEstimator.h>
 #include <frc/DriverStation.h>
+#include <cmath>
+#include <math.h>
 
 #include <units/length.h>
 #include <units/velocity.h>
@@ -46,22 +48,48 @@ class Drive : public frc2::SubsystemBase {
   void SwerveDrive(
     units::meters_per_second_t xspeed,
     units::meters_per_second_t yspeed,
-    units::radians_per_second_t rotspeed
+    units::radians_per_second_t rotspeed,
+    bool fieldRelative
   );
 
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 
+  //Kinematics Constants
+  units::meters_per_second_t kModuleMaxSpeed = 1_mps;
+  units::radians_per_second_t kRotMaxSpeed = 3.14_rad_per_s;
+  units::radians_per_second_squared_t kRotMaxAccel = 100_rad_per_s_sq;
+
+  //Robot Constants
+  units::meters_per_second_t kRobotMaxSpeed = 1_mps;
+  units::radians_per_second_t kRobotRotMaxSpeed = 3.14_rad_per_s;
+  
+  //Swerve Drive Kinematics
+  units::meter_t kRobotLength = 0_m;
+  units::meter_t kRobotWidth = 0_m;
+  frc::SlewRateLimiter<units::scalar> XRateLimiter{5 / 1_s};
+  frc::SlewRateLimiter<units::scalar> YRateLimiter{5 / 1_s};
+  frc::SlewRateLimiter<units::scalar> RotRateLimiter{5 / 1_s};
+
+  frc::SwerveDriveKinematics<4> m_kinematics{
+    frc::Translation2d{kRobotLength / 2, kRobotWidth / 2},
+    frc::Translation2d{kRobotLength / 2, -kRobotWidth / 2},
+    frc::Translation2d{-kRobotLength / 2, kRobotWidth / 2},
+    frc::Translation2d{-kRobotLength / 2, -kRobotWidth / 2}
+  };
+
   //Gyro
   frc::ADIS16470_IMU gyro;
+
+ 
 
   //Front Left Module
   rev::spark::SparkMax frontleftDriveMotor{OperatorConstants::frontleftDriveMotorID, rev::spark::SparkMax::MotorType::kBrushless};
   rev::spark::SparkMax frontleftRotMotor{OperatorConstants::frontleftRotMotorID, rev::spark::SparkMax::MotorType::kBrushless};
   ctre::phoenix6::hardware::CANcoder frontleftRotEncoder{OperatorConstants::frontleftRotEncoderID, "rio"};
   frc::PIDController flDrivePID{0.0, 0.0, 0.0};
-  frc::ProfiledPIDController<units::radians> flRotPID{0.0, 0.0, 0.0, {0.0_rad_per_s, 0.0_rad_per_s_sq}};
+  frc::ProfiledPIDController<units::radians> flRotPID{0.0, 0.0, 0.0, {kRotMaxSpeed, kRotMaxAccel}};
   frc::SimpleMotorFeedforward<units::meters> flDriveFF{0_V, 0_V / 1_mps, 0_V / 1_mps_sq};
   frc::SimpleMotorFeedforward<units::radians> flRotFF{0_V, 0_V / 1_rad_per_s};
 
@@ -70,7 +98,7 @@ class Drive : public frc2::SubsystemBase {
   rev::spark::SparkMax frontrightRotMotor{OperatorConstants::frontrightRotMotorID, rev::spark::SparkMax::MotorType::kBrushless};
   ctre::phoenix6::hardware::CANcoder frontrightRotEncoder{OperatorConstants::frontrightRotEncoderID, "rio"};
   frc::PIDController frDrivePID{0.0, 0.0, 0.0};
-  frc::ProfiledPIDController<units::radians> frRotPID{0.0, 0.0, 0.0, {0.0_rad_per_s, 0.0_rad_per_s_sq}};
+  frc::ProfiledPIDController<units::radians> frRotPID{0.0, 0.0, 0.0, {kRotMaxSpeed, kRotMaxAccel}};
   frc::SimpleMotorFeedforward<units::meters> frDriveFF{0_V, 0_V / 1_mps, 0_V / 1_mps_sq};
   frc::SimpleMotorFeedforward<units::radians> frRotFF{0_V, 0_V / 1_rad_per_s};
 
@@ -79,7 +107,7 @@ class Drive : public frc2::SubsystemBase {
   rev::spark::SparkMax backleftRotMotor{OperatorConstants::backleftRotMotorID, rev::spark::SparkMax::MotorType::kBrushless};
   ctre::phoenix6::hardware::CANcoder backleftRotEncoder{OperatorConstants::backleftRotEncoderID, "rio"};
   frc::PIDController blDrivePID{0.0, 0.0, 0.0};
-  frc::ProfiledPIDController<units::radians> blRotPID{0.0, 0.0, 0.0, {0.0_rad_per_s, 0.0_rad_per_s_sq}};
+  frc::ProfiledPIDController<units::radians> blRotPID{0.0, 0.0, 0.0, {kRotMaxSpeed, kRotMaxAccel}};
   frc::SimpleMotorFeedforward<units::meters> blDriveFF{0_V, 0_V / 1_mps, 0_V / 1_mps_sq};
   frc::SimpleMotorFeedforward<units::radians> blRotFF{0_V, 0_V / 1_rad_per_s};
 
@@ -88,8 +116,19 @@ class Drive : public frc2::SubsystemBase {
   rev::spark::SparkMax backrightRotMotor{OperatorConstants::backrightRotMotorID, rev::spark::SparkMax::MotorType::kBrushless};
   ctre::phoenix6::hardware::CANcoder backrightRotEncoder{OperatorConstants::backrightRotEncoderID, "rio"};
   frc::PIDController brDrivePID{0.0, 0.0, 0.0};
-  frc::ProfiledPIDController<units::radians> brRotPID{0.0, 0.0, 0.0, {0.0_rad_per_s, 0.0_rad_per_s_sq}};
+  frc::ProfiledPIDController<units::radians> brRotPID{0.0, 0.0, 0.0, {kRotMaxSpeed, kRotMaxAccel}};
   frc::SimpleMotorFeedforward<units::meters> brDriveFF{0_V, 0_V / 1_mps, 0_V / 1_mps_sq};
   frc::SimpleMotorFeedforward<units::radians> brRotFF{0_V, 0_V / 1_rad_per_s};
-
+ 
+  //Position Estimator
+  frc::SwerveDrivePoseEstimator<4> m_poseEstimator{m_kinematics, frc::Rotation2d{}, {
+    frc::SwerveModulePosition{(units::meter_t)(frontleftDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(frontleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(frontrightDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(frontrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(backleftDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(backleftRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)},
+    frc::SwerveModulePosition{(units::meter_t)(backrightDriveMotor.GetEncoder().GetPosition() * M_PI * 0.1016 / 8.14),
+      (units::radian_t)(backrightRotEncoder.GetAbsolutePosition().GetValueAsDouble() * M_PI * 2)}
+  }, frc::Pose2d{}};
 };
